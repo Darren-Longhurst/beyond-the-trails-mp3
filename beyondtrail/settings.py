66a9ca7db1e7f39d4b93a039fa04590589bb7b0e
Darren Longhurst <dmlonghurst@icloud.com>
2025-12-13 20:38:25 +0000

    Initial Heroku Deployment

--- beyondtrail/settings.py
+++ beyondtrail/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hxbz=$_*epn5!psthk02ixv^98yc^xs4@_012l93=stdb#06+m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
