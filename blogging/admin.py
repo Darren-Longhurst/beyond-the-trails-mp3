f2b4896b62ae773856af5aeb91e6b5984bec0d5f
Darren Longhurst <dmlonghurst@icloud.com>
2025-12-13 19:57:43 +0000

    Add base views

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from blogging.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass  # Customize admin options here if needed
+
+admin.site.register(Post, PostAdmin)
--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blogging.models import Post
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Beyond The Trails!")
+    template = 'blogging/index.html'
+    context = {
+        'page_title': 'Blog Home',
+    }
+
+    return render(request, template, context)
+
+def post_detail(request):
+    posts = Post.objects.all()
+    template = 'blogging/post_detail.html'
+    context = {
+        'page_title': 'Post Detail',
+        'posts': posts,
+    }
+    
+    return render(request, template, context)
+
+def post_detail(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return HttpResponse("Post not found", status=404)
+
+    template = 'blogging/post_detail.html'
+    context = {
+        'page_title': post.title,
+        'post': post,
+    }
+    
+    return render(request, template, context)
