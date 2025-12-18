6391c99f45e6593bb4b68a3d372d8d445dd9be58
Darren Longhurst <dmlonghurst@icloud.com>
2025-12-18 00:58:51 +0000

    Fixing error 500 on deploy

--- beyondtrail/settings.py
+++ beyondtrail/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'beyondtrail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'TEMPLATES'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
