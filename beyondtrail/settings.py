06db2eedb59215002e370a7c818680709a418372
Darren Longhurst <dmlonghurst@icloud.com>
2025-12-18 00:37:50 +0000

    Retry deploy again

--- beyondtrail/settings.py
+++ beyondtrail/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',
+ALLOWED_HOSTS = ['bike-packing-blog.herokuapp.com',
                  '127.0.0.1',]
 
 
