975a04d509111224decc1d43012fb8df2e52dd2d
Darren Longhurst <dmlonghurst@icloud.com>
2025-12-21 22:56:50 +0000

    Install crispy forms and post request from comments

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -26,11 +28,25 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.filter(approved=True).order_by('created_at')
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+            messages.success(request, "Your comment has been submitted and is awaiting approval.")
+
+    comment_form = CommentForm()
 
     return render(
         request,
         "blogging/post_detail.html",
-        {"post": post},
+        {"post": post, "comments": comments, "comment_count": comment_count, "comment_form": comment_form},
     )
 """
 def index(request):
